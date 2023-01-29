d36d39366b82cc2831e0771753abdae285ad1316
dzikoysk <dzikoysk@dzikoysk.net>
2023-01-29 14:45:59 +0100

    GH-5 Release to Maven Central (Resolve #5)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,35 +3,92 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat.*
 import org.gradle.api.tasks.testing.logging.TestLogEvent.*
 
 group = "net.dzikoysk"
-version = "1.0.3"
+version = "1.1.0"
+description = "Exposed Upsert | Exposed extension for upsert operations"
 
 plugins {
-    kotlin("jvm") version "1.5.21"
+    kotlin("jvm") version "1.8.0"
+    `java-library`
     application
     jacoco
+    signing
     `maven-publish`
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
 }
 
 publishing {
     repositories {
         maven {
+            name = "reposilite-repository"
+            url = uri("https://maven.reposilite.com/${if (version.toString().endsWith("-SNAPSHOT")) "snapshots" else "releases"}")
+
             credentials {
-                username = property("mavenUser") as String
-                password = property("mavenPassword") as String
+                username = getEnvOrProperty("MAVEN_NAME", "mavenUser")
+                password = getEnvOrProperty("MAVEN_TOKEN", "mavenPassword")
             }
-            name = "panda-repository"
-            url = uri("https://repo.panda-lang.org/releases")
         }
     }
-    publications {
-        create<MavenPublication>("library") {
-            from(components.getByName("java"))
+    publishing {
+        publications {
+            create<MavenPublication>("library") {
+                pom {
+                    name.set(project.name)
+                    description.set(project.description)
+                    url.set("https://github.com/reposilite-playground/exposed-upsert")
+
+                    licenses {
+                        license {
+                            name.set("The Apache License, Version 2.0")
+                            url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                        }
+                    }
+                    developers {
+                        developer {
+                            id.set("dzikoysk")
+                            name.set("dzikoysk")
+                            email.set("dzikoysk@dzikoysk.net")
+                        }
+                    }
+                    scm {
+                        connection.set("scm:git:git://github.com/reposilite-playground/exposed-upsert.git")
+                        developerConnection.set("scm:git:ssh://github.com/reposilite-playground/exposed-upsert.git")
+                        url.set("https://github.com/reposilite-playground/exposed-upsert.git")
+                    }
+                }
+
+                from(components.getByName("java"))
+            }
+        }
+    }
+
+    if (findProperty("signing.keyId").takeIf { it?.toString()?.trim()?.isNotEmpty() == true } != null) {
+        signing {
+            sign(publishing.publications.getByName("library"))
+        }
+    }
+}
+
+tasks.register("release") {
+    dependsOn(
+        "clean", "build",
+        "publishAllPublicationsToReposilite-repositoryRepository",
+        "publishAllPublicationsToSonatypeRepository",
+        "closeAndReleaseSonatypeStagingRepository"
+    )
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            username.set(getEnvOrProperty("SONATYPE_USER", "sonatypeUser"))
+            password.set(getEnvOrProperty("SONATYPE_PASSWORD", "sonatypePassword"))
         }
     }
 }
 
 jacoco {
-    toolVersion = "0.8.7"
+    toolVersion = "0.8.8"
 }
 
 repositories {
@@ -39,15 +96,15 @@ repositories {
 }
 
 dependencies {
-    val exposed = "0.33.1"
+    val exposed = "0.41.1"
     implementation("org.jetbrains.exposed:exposed-core:$exposed")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposed")
 
-    testImplementation("com.h2database:h2:1.4.199") // 1.4.200 is broken
+    testImplementation("com.h2database:h2:2.1.214")
     testImplementation("org.xerial:sqlite-jdbc:3.36.0.2")
-    testImplementation("mysql:mysql-connector-java:8.0.25")
+    testImplementation("mysql:mysql-connector-java:8.0.28")
     testImplementation("org.mariadb.jdbc:mariadb-java-client:2.7.3")
-    testImplementation("org.postgresql:postgresql:42.2.23.jre7")
+    testImplementation("org.postgresql:postgresql:42.2.27")
 
     val testcontainers = "1.16.0"
     testImplementation("org.testcontainers:postgresql:$testcontainers")
@@ -61,9 +118,19 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.2")
 
-    val logback = "1.2.5"
+    val logback = "1.2.9"
     testImplementation("ch.qos.logback:logback-core:$logback")
     testImplementation("ch.qos.logback:logback-classic:$logback")
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 tasks.withType<KotlinCompile> {
@@ -91,11 +158,10 @@ tasks.test {
 
 tasks.jacocoTestReport {
     reports {
-        html.isEnabled = false
-        csv.isEnabled = false
-
-        xml.isEnabled = true
-        xml.destination = file("$buildDir/reports/jacoco/report.xml")
+        html.required.set(false)
+        csv.required.set(false)
+        xml.required.set(true)
+        xml.outputLocation.set(file("$buildDir/reports/jacoco/report.xml"))
     }
 
     onlyIf {
@@ -141,3 +207,7 @@ val testCoverage by tasks.registering {
     tasks["jacocoTestReport"].mustRunAfter(tasks["test"])
     tasks["jacocoTestCoverageVerification"].mustRunAfter(tasks["jacocoTestReport"])
 }
+
+fun getEnvOrProperty(env: String, property: String): String? =
+    System.getenv(env) ?: findProperty(property)?.toString()
+
